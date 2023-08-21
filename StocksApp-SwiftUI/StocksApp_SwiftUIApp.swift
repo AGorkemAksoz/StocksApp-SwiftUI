7636564af39a514886184b1296c676479321b77e
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2023-08-21 09:59:32 +0300

    Initial Commit

---
+++ StocksApp-SwiftUI/StocksApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp_SwiftUIApp.swift
+//  StocksApp-SwiftUI
+//
+//  Created by Gorkem on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
